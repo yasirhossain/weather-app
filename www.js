4637d8e6ee906a04a2289c29df9fc1674717e32d
Yasir Hossain <yhossain@gmail.com>
2017-05-24 01:15:25 -0700

    pointing to public folder for deploy

--- www.js
+++ www.js
@@ -4,11 +4,11 @@ const port = process.env.PORT || 5000
 const app = express()
 
 // serve static assets normally
-app.use(express.static(__dirname + '/dist'))
+app.use(express.static(__dirname + '/public'))
 
 // Handles all routes so you do not get a not found error
 app.get('*', function (request, response){
-    response.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
+    response.sendFile(path.resolve(__dirname, 'public', 'index.html'))
 })
 
 app.listen(port)
